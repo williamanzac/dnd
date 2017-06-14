d61079cca5421d2bc1ce65b9b4e18325b71bce09
tonyfountaine <tony.fountaine@trineo.co.nz>
2017-06-14 20:40:14 +1200

    Removed commented code

--- src/main/resources/js/viewModels/heightWeight.js
+++ src/main/resources/js/viewModels/heightWeight.js
@@ -17,54 +17,11 @@ define(['jquery', 'knockout', 'Modal', '../components/heightWeight', 'knockstrap
 		self.createHeightWeight = function() {
 			self.hideNewHeightWeightModal();
 			self.newScore().create();
-/*			var myArray = /(\d+)'(\d+)"/g.exec(self.newScore().baseHeight());
-			var baseHeight = parseInt(myArray[1]) * 12 + parseInt(myArray[2]);
-			console.log(baseHeight);
-			var data = {
-				"type" : self.newScore().type(),
-				"baseHeightInch" : baseHeight,
-				"heightModifier" : self.newScore().heightModifier(),
-				"baseWeightLB" : parseInt(self.newScore().baseWeightLB()),
-				"weightModifier" : self.newScore().weightModifier()
-			};
-			$.ajax({
-				method : "POST",
-				data : JSON.stringify(data),
-				url : "/tools/heightWeights",
-				contentType : "application/json",
-				dataType : "json"
-			}).done(function() {
-				self.getHeightWeights();
-				self.newScore(new HeightWeight());
-			});
-			*/
 		}
 
 		self.updateHeightWeight = function() {
 			self.hideEditHeightWeightModal();
 			self.newScore().update();
-/*			var myArray = /(\d+)'(\d+)"/g.exec(self.newScore().baseHeight());
-			var baseHeight = parseInt(myArray[1]) * 12 + parseInt(myArray[2]);
-			console.log(baseHeight);
-			var data = {
-				"id" : parseInt(self.newScore().id),
-				"type" : self.newScore().type(),
-				"baseHeightInch" : baseHeight,
-				"heightModifier" : self.newScore().heightModifier(),
-				"baseWeightLB" : parseInt(self.newScore().baseWeightLB()),
-				"weightModifier" : self.newScore().weightModifier()
-			};
-			$.ajax({
-				method : "PUT",
-				data : JSON.stringify(data),
-				url : "/tools/heightWeights",
-				contentType : "application/json",
-				dataType : "json"
-			}).done(function() {
-				self.getHeightWeights();
-				self.newScore(new HeightWeight());
-			});
-			*/
 		}
 
 		self.getHeightWeights();
